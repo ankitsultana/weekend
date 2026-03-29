29fa698194a7fa433462da466f4a5d1af7fa10be
Ankit Sultana <ankitsultana@gmail.com>
2026-03-28 18:36:44 -0700

    cleanup

--- rust-experiments/school/src/seg_tree.rs
+++ rust-experiments/school/src/seg_tree.rs
@@ -1,20 +1,45 @@
-#[allow(dead_code)]
-fn add2(x: i32, y: i32) -> i32 {
-    return x + y;
+pub struct SegNode<V> {
+    pub value: Box<V>,
+    pub left: Option<Box<SegNode<V>>>,
+    pub right: Option<Box<SegNode<V>>>,
 }
 
-pub struct SegTree {
-    pub value: i32,
-    pub left: Option<Box<SegTree>>,
-    pub right: Option<Box<SegTree>>,
-}
-
-impl SegTree {
-    pub fn new(value: i32) -> Self {
-        return SegTree {
+impl<V: std::fmt::Display> SegNode<V> {
+    pub fn new(value: Box<V>) -> Self {
+        return Self {
             value: value,
             left: None,
             right: None,
         };
+    }
+
+    pub fn new_with_options(value: Box<V>, left: Option<Box<SegNode<V>>>, right: Option<Box<SegNode<V>>>) -> Self {
+        return Self {
+            value: value,
+            left: left,
+            right: right,
+        };
+    }
+
+    pub fn new_with_inputs(value: Box<V>, left: Box<SegNode<V>>, right: Box<SegNode<V>>) -> Self {
+        return Self {
+            value: value,
+            left: Some(left),
+            right: Some(right),
+        };
+    }
+
+    pub fn print(&self) {
+        println!("SegNode({})", self.value);
+    }
+
+    pub fn print_r(&self) {
+        self.print();
+        if let Some(ref l) = self.left {
+            l.print_r();
+        }
+        if let Some(ref r) = self.right {
+            r.print_r();
+        }
     }
 }
